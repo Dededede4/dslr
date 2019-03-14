e1eee5d9da3a283c54895992f2a20734a08d192f
jb255 <jaustry@student.42.fr>
2019-03-14 15:37:14 +0100

    bug ?

---
+++ logreg_train2.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+np.set_printoptions(formatter={'float': lambda x: "{0:0.6f}".format(x)})
+
+data = pd.read_csv('dataset_train.csv')
+data = data.dropna()
+
+X = data.values[:, 6:8]
+y = data.values[:, 1:2]
+print(X.shape)
+print(y.shape)
+
+theta = np.zeros(3)
+
+def sigmoid(z):
+    return (1 / (1 + np.exp(-z)))
+
+print(sigmoid(0.000))
+print(sigmoid(30.0004))
+print(sigmoid(-100.005))
+
+def predict(X, theta):
+      return(sigmoid(np.dot(X, theta)))
+
+def cost(X, y, theta):
+    return((-1 / X.shape[0]) * np.sum(y * np.log(predict(X, theta)) + (1 - y) * np.log(1 - predict(X, theta))))
+
+X = np.c_[np.ones(X.shape[0]), X]
+print (X)
+
+theta = np.zeros(3, dtype=float)
+cost(X, y, theta)
